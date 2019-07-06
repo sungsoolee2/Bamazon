d4186caed0ded89a377aa515d5dc5bc877b43948
Sung Lee <sungsoolee2@hotmail.com>
2019-07-06 16:49:45 -0500

    Second Draft

--- Customer.js
+++ Customer.js
@@ -23,17 +23,28 @@ connection.connect(function (err) {
 });
 
 
-function start() {
+function displayInventory() {
     //Display store items , id ,and price//
 
-    connection.query("SELECT * FROM products", function (err, res) {
+    connection.query("SELECT * FROM products", function (err, result) {
         if (err) throw err;
+        var inventory = "";
+        for (var i = 0; i < result.length; i++) {
+            inventory = "";
+            inventory += "Item ID: " + result[i].item_id + " | ";
+            inventory += "Product Name: " + result[i].product_name + " | ";
+            inventory += "Department: " + result[i].department_name + " | ";
+            inventory += "Price : " + result[i].price + " | ";
 
-        for (var i = 0; i < res.length; i++) {
-            console.log("Item ID: " + res[i].item_id + " | " + "Product Name: " + res[i].product_name + " | " + "Price : " + res[i].price)
+            console.log(inventory);
         }
-    });
+        //prompt user function call//
+        start();
 
+    });
+}
+
+function start() {
     // prompt for info about the item being put up for auction
     inquirer
         .prompt([
@@ -41,89 +52,104 @@ function start() {
                 name: "itemid",
                 type: "input",
                 message: "Please enter the ID of the Product to buy.",
-
-                // validate input is number from 1-10
-                validate: function (value) {
-                    if (!isNaN(value) && (value > 0 && value <= 10)) {
-                        return true;
-                    } else {
-                        console.log('   Please enter a number from 1-10');
-                        return false;
-                    }
-                }
-            },
-
+                filter: Number
+                
+                },
             {
                 name: "quantity",
                 type: "input",
                 message: "Please enter Quantity of the product to buy.",
-
-                // validate input is number from 1-10
-                validate: function (value) {
-                    if (!isNaN(value)) {
-                        return true;
-                    } else {
-                        console.log('   Please enter a valid number!');
-                        return false;
-                    }
-                }
+                filter: Number
+                
             }
 
         ]).then(function (answer) {
             //assigning var to item, quant and total price//
             var itemSelected = (answer.itemid);
-            // console.log(itemSelected);
+            console.log(itemSelected);
 
             var quantSelected = parseInt(answer.quantity);
-            // console.log(quantSelected);
-            // console.log(res[itemSelected].price);
-            var totalPrice = parseFloat(((res[itemSelected].price) * quantSelected).toFixed(2));
+            console.log(quantSelected);
 
-            //check if in stock is sufficent to fill order//
-            if (res[itemSelected].stock_quantity >= quantSelected) {
+            // Verifiy that id entered is in the database and is a valid id//
+            connection.query("SELECT * FROM products WHERE ?", [{item_id: itemSelected}], function (err, result)
+                 {
+                    if (err) throw err;
+                    console.log(JSON.stringify(result));
+                    if (result.length === 0) {
+                        console.log("Please enter a valid product ID number!");
+                        displayInventory();
 
-                //update database and fill the order //
+                    } else {
 
-                connection.query("UPDATE products SET ? WHERE ?", [{ stock_quantity: (res[itemSelected].stock_quantity - quantSelected) }, { item_id: answer.itemid }],
-                    function (err, result) {
-                        if (err) throw err;
-                        console.log("Your order has been filled. Your total purchase amount is $" + totalPrice);
-
-                    });
-
-            } else {
-                console.log("Insufficent stock available!");
-            }
-
-            //ask if customer wants another item//
-            anotherPurchase();
-        });
-
+                        var product = result[0];
+                        console.log(product);
+                        // check to see if quantity selected is in stock and then fill order if it is//
+                        if (quantSelected <= product.stock_quantity) {
+                        
+                            console.log("You purchased "+ quantSelected+ "  "+ product.product_name)
+                            console.log("Your order has been filled. Your total purchase amount is $" + parseFloat(product.price * quantSelected).toFixed(2))
+                            
+                            connection.query("UPDATE products SET stock_quantity = " + (product.stock_quantity - quantSelected)
+                                + " WHERE item_id = " + itemSelected), function (err, result) {
+                                     if (err) throw err;
+                                }
+                        } else {
+                            console.log("Insufficent stock available!");
+                            displayInventory();
+                        }
+                        anotherPurchase();
+                    }
+                })       
+        })
 }
+
 
 function anotherPurchase() {
     inquirer
-        .prompt([
+        .prompt(
             {
-                name: "purchase",
-                type: "yesorno",
-                message: "Would you like to purchase another item?"
-            }
-        ]).then(function (answer) {
-            if (answer.yesorno) {
-                start();
-            } else {
-                console.log(" Have a nice day!");
-
-            }
-        });
-}
-
+                name: "action",
+                type: "list",
+                message: "Would you like to purchase another item?",
+                choices: [
+                    "YES", 
+                    "NO",
+                ]
+            })
+        .then(function(answer) {
+                switch (answer.action) {
+                case "YES":
+                  displayInventory();
+                  break;
+          
+                case "NO":
+                  console.log("Thank you for shopping at Bamazon!")
+                  connection.end();
+                  break;
+                }
+            });
+        }
 //run to display items and prompt user for item and quantity
-start();
+displayInventory();
 
-
-
+// // validate input is number from 1-10
+// validate: function (value) {
+//     if (!isNaN(value) && (value > 0 && value <= 10)) {
+//         return true;
+//     } else {
+//         console.log('   Please enter a number from 1-10');
+//         return false;
+//     }
+// // validate input is number from 1-10
+// validate: function (value) {
+//     if (!isNaN(value)) {
+//         return true;
+//     } else {
+//         console.log('   Please enter a valid number!');
+//         return false;
+//     }
+// }
 
 
 
